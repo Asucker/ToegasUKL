f1bb4e0e477d22bd2d85f94438a825d1bea8ecaa
Asucker <thecrystalclods1@gmail.com>
2023-04-06 09:31:26 +0700

    hallo

---
+++ app/src/main/java/com/example/uklrecycle/ViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.uklrecycle
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+@Suppress("DEPRECATION")
+internal class ViewPagerAdapter(
+    var context: Context,
+    fm: FragmentManager,
+    var totalTabs: Int
+) :
+    FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                MyFriendsAddFragment()
+            }
+            1 -> {
+                MyFriendsFragment()
+            }
+
+            else -> getItem(position)
+        }
+    }
+    override fun getCount(): Int {
+        return totalTabs
+    }
+}
